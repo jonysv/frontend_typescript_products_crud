51bb6b368d780906dc7c0c64df52b4c163439e67
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-26 18:37:46 -0600

    09

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,8 +1,22 @@
+import { safeParse } from 'valibot'
+import { DraftProductSchema } from "../types"
 
 type ProductData = {
     [k: string]: FormDataEntryValue;
 }
 
 export async function addProduct(data : ProductData) {
-    console.log(data)
+    try {
+        const result = safeParse(DraftProductSchema, {
+            name: data.name,
+            price: data.price
+        })
+        if(result.success) {
+
+        } else {
+            throw new Error('Datos no válidos')
+        }
+    } catch (error) {
+        console.log(error)
+    }
 }
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+import { object, string, number } from 'valibot'
+
+export const DraftProductSchema = object({
+    name: string(),
+    price: number()
+})
